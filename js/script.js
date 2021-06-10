1bc30aea12ec705ad7fbf68446f6ebc73a344d57
Jamal <jamal.rzayev@gmail.com>
2021-06-10 17:07:32 +0400

    update

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.menu'),
+      closeElem = document.querySelector('.menu__close'),
+      overlay = document.querySelector('.menu__overlay');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('active')
+});
+
+const percentages = document.querySelectorAll('.skills__indicator-percentage'),
+      bars = document.querySelectorAll('.skills__indicator-bar');
+
+percentages.forEach( (item, i) => {
+    bars[i].style.width = item.innerHTML;
+});
